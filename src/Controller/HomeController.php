4d84ae61c3dc097dce6627e2c8afe79ac4855771
adrienbouthet <adrien.bouthet@ynov.com>
2019-09-09 16:16:32 +0200

    inital commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends  AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+ public function Index(){
+    return $this->render('pages/home.html.twig');
+ }
+}
